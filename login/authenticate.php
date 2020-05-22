d1e3eacb35601b87edeaec9988ab5d8c8990e228
Eli Kimchi <rabu8770@gmail.com>
2020-05-22 13:11:30 -0700

    login system

--- login/authenticate.php
+++ login/authenticate.php
@@ -5,7 +5,7 @@
 
     $id_token = $_GET['idtoken'];
 
-    $client = new Google_Client(['client_id' => '570971308489-6golgnijuhqlvon7s2av7getpv2j4h4d.apps.googleusercontent.com']);
+    $client = new Google_Client(['client_id' => '557693377850-dl01iqofmgd9ks1anca969bu76nkn08c.apps.googleusercontent.com']);
     $guzzleClient = new \GuzzleHttp\Client(array( 'curl' => array( CURLOPT_SSL_VERIFYPEER => false, ), ));
     $client->setHttpClient($guzzleClient);
 
@@ -14,9 +14,9 @@
     if ($payload) {
         $email = $payload['email'];
         //verify student
-        if($db->query("SELECT id FROM `students` WHERE `email` = '$email'")->num_rows) {
+        if($db->query("SELECT id FROM `users` WHERE `email` = '$email'")->num_rows) {
             setcookie('login_key', $id_token, time() + (86400 * 5), "/");
-            $db->query("UPDATE `students` SET `login_key` = '$id_token' WHERE `email` = '$email'");
+            $db->query("UPDATE `users` SET `login_key` = '$id_token' WHERE `email` = '$email'");
             echo '2';
         } elseif ($db->query("SELECT id FROM `admin` WHERE `email` = '$email'")->num_rows) {
             setcookie('login_key', $id_token, time() + (86400 * 3), "/");
